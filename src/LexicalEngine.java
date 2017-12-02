568fe072a2e8a95b4c4f172c3a02c9927d031d36
Oussama LightRay <SakasakyRay@Gmail.com>
2017-12-02 17:23:12 +0100

    Implemented Lexical Engine

--- src/LexicalEngine.java
+++ src/LexicalEngine.java
@@ -1,98 +1,413 @@
-import token.IdToken;
-import token.Token;
+import token.*;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.regex.Pattern;
+
+import static jdk.nashorn.internal.runtime.JSType.isNumber;
 
 /**
  * Created by ${} on 18/11/2017.
  */
 public class LexicalEngine
 {
+    private final Pattern idPattern = Pattern.compile("[a-z]|[A-Z](_?([A-Za-z]|[0-9])+)*");
+    Pattern dataPattern = Pattern.compile("(\\\".*\\\")|([0-9]+(\\.[0-9]+)?)");
+    Pattern numberPatter = Pattern.compile("([0-9]+(\\.[0-9]+)?)");
+
+
     private final String[] keywords = {
             "Start_Program",
-            "Int_Number",
-            "Real_Number",
+            "End_Program",
+            "ShowMes",
+            "ShowVal",
             "Give",
             "Affect",
-            "to",
             "If",
-            "--",
             "Else",
+            ";;",
+            "to",
             "Start",
-            "Finish",
-            ";;"
+            "Finish"
+    };
+
+    private final String[] varTypes = {
+            "Int_Number",
+            "Real_Number"
     };
 
     private final String[] symbols = {
             ":",
-            ".",
+            "--",
             ","
     };
+
+    private final String[] arithmeticOp = {
+            ">",
+            "<",
+            "==",
+            "<=",
+            ">="
+    };
+
     private String sourceCode;
-    private ArrayList<Token> tokenSource;
+    private ArrayList<Token> tokenSource = new ArrayList<>();
+    private String buffer = "";
 
     private int positionTeteLecture = 0;
 
     public LexicalEngine()
     {
         sourceCode = "";
-        tokenSource = new ArrayList<Token>();
+        buffer = "";
+        clear();
+    }
+
+    private boolean EOF()
+    {
+        return positionTeteLecture >= sourceCode.length();
     }
 
     private char nextChar()
     {
+        buffer += currentChar();
         positionTeteLecture++;
         return currentChar();
     }
 
     private char currentChar()
     {
-        if(positionTeteLecture >= sourceCode.length())
+        if (positionTeteLecture >= sourceCode.length())
             return '$';
         return sourceCode.charAt(positionTeteLecture);
     }
 
     public void setSourceCode(String src)
     {
+        positionTeteLecture = 0;
         this.sourceCode = src;
     }
+
     public ArrayList<Token> getTokenSource()
     {
         performAnalysis();
         return tokenSource;
     }
 
-    private boolean isKeyWord(String word)
+    public void clear()
     {
-        return false;
+        tokenSource.clear();
+        buffer = "";
+        positionTeteLecture = 0;
     }
-
-    private boolean isIdentifier(String word)
-    {
-        return false;
-    }
-
 
     private ArrayList<String> reconstructLines(String sourceCode)
     {
         // divise en lignes et ignore les commentaires, if needed
         String[] lines = sourceCode.split("\\n");
 
+
         ArrayList<String> linesList = new ArrayList<>();
 
-        for(String line : lines)
-            if(!line.startsWith("//."))
+        for (String line : lines)
+            if (!line.startsWith("//."))
                 linesList.add(line);
 
         return linesList;
     }
 
-    private void performAnalysis()
+    private boolean performAnalysis()
     {
+        clear();
         System.out.println("Performing lexical analysis");
 
         char c = currentChar();
+        while (!EOF())
+        {
+            boolean proceed = true; // si vrai lire le prochain char à la fin
+
+            // verifier commentaires
+            if(!skipComments())
+                return false;
+            c = currentChar();
+            // lecture
+            if (isSeparator(c))
+            {
+                // Check previous
+                if (buffer.isEmpty())
+                {
+                    // nothing
+                }
+                else if (isKeyword(buffer))
+                {
+                    tokenSource.add(new KeywordToken(buffer));
+                }
+                else if (isArithmeticOp(buffer))
+                {
+                    tokenSource.add(new ArithmeticToken(buffer));
+                }
+                else if (isVarType(buffer))
+                {
+                    tokenSource.add(new VarTypeToken(buffer));
+                }
+                else if (isNumber(buffer))
+                {
+                    tokenSource.add(new DataToken(buffer));
+                }
+                else if (isIdentifier(buffer))
+                {
+                    tokenSource.add(new IdToken(buffer));
+                }
+                else
+                {
+                    return logError("Unidentified");
+                }
+
+                // Now check the separator
+                switch (c)
+                {
+                    case '<':
+                    case '>':
+                    case '=':
+                        if (!readArithmeticOp())
+                            return logError("Illegal operator Expected ");
+                        proceed = false;
+                        break;
+                    case ':':
+                        tokenSource.add(new SymbolToken(":"));
+                        break;
+                    case ';':
+                        c = nextChar();
+                        if (c == ';')
+                            tokenSource.add(new KeywordToken(";;"));
+                        else
+                            return logError("Unexpected token ';' found");
+                        break;
+                    case '-':
+                        c = nextChar();
+                        if (c == '-')
+                            tokenSource.add(new SymbolToken("--"));
+                        else
+                            return logError("Unexpected token ';' found");
+                        break;
+                    case ',':
+                        tokenSource.add(new SymbolToken(","));
+                        break;
+                    case '"':
+                        if (!readString())
+                            return logError("'\"' Expected ");
+                        proceed = false;
+                        break;
+                    default:    // whitespace ignored
+                }
+                if (proceed)
+                    c = nextChar();
+                else
+                    c = currentChar();
+                buffer = ""; // clear buffer
+            }
+            else
+            {
+                c = nextChar();
+            }
+        }
+
+        return true; // correct
+    }
+
+    private boolean skipComments()
+    {
+        char c = currentChar();
+        if(c == '/')
+        {
+            c = nextChar();
+            if(c == '/')
+            {
+                c = nextChar();
+                if(c == '.')
+                {
+                    // skip to nextLine
+                    while (nextChar() != '\n');
+                    nextChar();
+                    buffer = "";
+                    return true;
+                }
+            }
+        }
+        else
+        {
+            return true;
+        }
+
+        return logError("Illegal caracter");
+    }
+
+    private boolean readArithmeticOp()
+    {
+        char c = currentChar();
+        if (c == '<' || c == '>' || c == '=')
+        {
+            char next_c = nextChar();
+            if (next_c == '=')
+            {
+                next_c = nextChar();
+                tokenSource.add(new ArithmeticToken(c + "="));
+            }
+            else
+            {
+                tokenSource.add(new ArithmeticToken("" + c));
+            }
+            return true;
+        }
+        return false;   // should be impossible ...
+    }
+
+    private boolean readString()
+    {
+        char c;
+
+        do
+        {
+            c = nextChar();
+        } while (c != '"' && !EOF());
+        nextChar();
+        tokenSource.add(new DataToken(buffer));
+        buffer = "";
+        return true;
+    }
+
+    private boolean isArithmeticOp(String buffer)
+    {
+        return TextProcessor.compare(buffer, ">") ||
+                TextProcessor.compare(buffer, "<") ||
+                TextProcessor.compare(buffer, "==") ||
+                TextProcessor.compare(buffer, "<=") ||
+                TextProcessor.compare(buffer, ">=");
+    }
+
+    private boolean isIdentifier(String buffer)
+    {
+        return (idPattern.matcher(buffer).matches());
+    }
+
+    private boolean isVarType(String buffer)
+    {
+        return TextProcessor.compare(buffer, "Int_Number")
+                || TextProcessor.compare(buffer, "Real_Number");
+    }
+
+    private boolean isKeyword(String buffer)
+    {
+        return TextProcessor.compare(buffer, "Start_Program")
+                || TextProcessor.compare(buffer, "End_Program")
+                || TextProcessor.compare(buffer, "ShowMes")
+                || TextProcessor.compare(buffer, "ShowVal")
+                || TextProcessor.compare(buffer, "Give")
+                || TextProcessor.compare(buffer, "Affect")
+                || TextProcessor.compare(buffer, "If")
+                || TextProcessor.compare(buffer, "Else")
+                || TextProcessor.compare(buffer, ";;")
+                || TextProcessor.compare(buffer, "to")
+                || TextProcessor.compare(buffer, "Start")
+                || TextProcessor.compare(buffer, "Finish");
+    }
+
+    private boolean isSymbol(String buffer)
+    {
+        return false;
+    }
+
+    private boolean isDigit(char c)
+    {
+        return c == '0' ||
+                c == '1' ||
+                c == '2' ||
+                c == '3' ||
+                c == '4' ||
+                c == '5' ||
+                c == '6' ||
+                c == '7' ||
+                c == '8' ||
+                c == '9';
+    }
+
+    private boolean isNumber(String buffer)
+    {
+        return numberPatter.matcher(buffer).matches();
+//        for (int i = 0; i < buffer.length(); i++)
+//        {
+//            if (!isDigit(buffer.charAt(i)))
+//            {
+//                return false;
+//            }
+//        }
+//        return true;
+    }
+
+    private boolean isSeparator(char c)
+    {
+        return (c == ' '
+                || c == '\n'
+                || c == '\t'
+                || c == ','
+                || c == ':'
+                || c == ';'
+                || c == '-'
+                || c == '>'
+                || c == '<'
+                || c == '='
+                || c == '"');
+    }
+
+
+    private boolean logError(String s)
+    {
+        System.out.println("ERROR : " + s);
+        return false;
+    }
+
+    private void performAnalysis2()
+    {
+        clear();
+        System.out.println("Performing lexical analysis");
+
+        Pattern idPattern = Pattern.compile("[a-z]|[A-Z](_?([A-Za-z]|[0-9])+)*");
+        Pattern dataPattern = Pattern.compile("(\\\".*\\\")|([0-9]+(\\.[0-9]+)?)");
+
+        ArrayList<String> lines = reconstructLines(sourceCode);
+
+        for (String line : lines)
+        {
+            String[] words = line.split(" ");
+
+            for (String word : words)
+            {
+                if (Arrays.asList(keywords).contains(word))
+                {
+                    tokenSource.add(new KeywordToken(word));
+                }
+                else if (Arrays.asList(symbols).contains(word))
+                {
+                    tokenSource.add(new SymbolToken(word));
+                }
+                else if (Arrays.asList(varTypes).contains(word))
+                {
+                    tokenSource.add(new VarTypeToken(word));
+                }
+                else if (Arrays.asList(arithmeticOp).contains(word))
+                {
+                    tokenSource.add(new ArithmeticToken(word));
+                }
+                else if (dataPattern.matcher(word).matches())
+                {
+                    tokenSource.add(new DataToken(word));
+                }
+                else if (idPattern.matcher(word).matches())
+                {
+                    tokenSource.add(new IdToken(word));
+                }
+            }
+        }
 
         System.out.println("Found " + tokenSource.size() + " tokens");
     }

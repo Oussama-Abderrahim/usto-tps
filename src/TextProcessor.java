3e19ee3d032d73e71bf4f1c1d5e88fdd7c78113f
Oussama LightRay <SakasakyRay@Gmail.com>
2017-12-02 19:03:52 +0100

    added char functions ( isDigit, isLower, isUpper )

--- src/TextProcessor.java
+++ src/TextProcessor.java
@@ -14,4 +14,29 @@ public class TextProcessor
         return false;
     }
 
+    public static boolean isDigit(char c)
+    {
+        return c == '1' ||
+                c == '2' ||
+                c == '3' ||
+                c == '4' ||
+                c == '5' ||
+                c == '6' ||
+                c == '7' ||
+                c == '8' ||
+                c == '9' ||
+                c == '0';
+    }
+
+    public static boolean isUpperCaseLetter(char c)
+    {
+        return Character.getNumericValue(c) >= Character.getNumericValue('A')
+                && Character.getNumericValue(c) <= Character.getNumericValue('Z');
+    }
+
+    public static boolean isLowerCaseLetter(char c)
+    {
+        return Character.getNumericValue(c) >= Character.getNumericValue('a')
+                && Character.getNumericValue(c) <= Character.getNumericValue('z');
+    }
 }

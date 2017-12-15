fd73b0f005ce1933fe33728b3ec32c4f52354893
Oussama LightRay <SakasakyRay@Gmail.com>
2017-12-15 21:28:29 +0100

    Added Instruction Objects in syntaxAnalysis

---
+++ src/instruction/InstructionType.java
@@ -0,0 +1,14 @@
+package instruction;
+
+/**
+ * Created by ${} on 15/12/2017.
+ */
+public enum InstructionType
+{
+    START_PROGRAM,
+    END_PROGRAM,
+    AFFECT_VALUE,
+    AFFECT_VAR,
+    SHOW_MSG,
+    IF_STATEMENT, VAR_DECL, BEGIN, FINISH, ELSE_STATEMENT, SHOW_VAR
+}

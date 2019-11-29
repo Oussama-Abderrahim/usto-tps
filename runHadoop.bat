9de2aa7ab34d116b9e903075dcc6158cabf95cce
Oussama Abderrahim <OussamaLgt@Gmail.com>
2019-11-29 21:45:12 +0100

    Add scripts to automate running mapreduce programs

--- runHadoop.bat
+++ runHadoop.bat
@@ -6,7 +6,7 @@ hadoop fs -put ./inputs/%1/%2.txt /bdms/inputs/%1/%2.txt &&^
 echo "Running Jar" && ^
 hadoop jar ./MapReduce.jar %1.%2 /bdms/inputs/%1 /bdms/outputs/%1 &&^
 echo "Clearning local output folder" && ^
-rmdir /s /q ./outputs/%1/%2 &^
+rmdir /s /q "./outputs/%1/%2" &^
 echo "Copy results from HDFS" && ^
 hadoop fs -get /bdms/outputs/%1/%2 ./outputs/%1 &&^
 code ./outputs/%1/%2^

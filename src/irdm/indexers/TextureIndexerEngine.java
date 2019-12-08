8d97c12eb46188dbcce70f530b23a0d33f59ea5b
ayasunshin8 <ayaichrack@gmail.com>
2019-12-08 08:15:15 +0100

    wubba lubba dub dub

--- src/irdm/indexers/TextureIndexerEngine.java
+++ src/irdm/indexers/TextureIndexerEngine.java
@@ -1,12 +1,16 @@
 package irdm.indexers;
 
 import irdm.descriptors.TextureDescriptor;
+
+import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Comparator;
 
 public class TextureIndexerEngine implements IndexerEngine {
 
+    public static final int L = 32;
     public static TextureIndexerEngine instance = null;
 
     public static TextureIndexerEngine getInstance() {
@@ -22,7 +26,15 @@ public class TextureIndexerEngine implements IndexerEngine {
      */
     @Override
     public TextureDescriptor getDescriptor(BufferedImage image) {
-        /// TODO: Aya.
+        int n = image.getWidth(), m = image.getHeight();
+        double[][] grays = new double[n][m];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                Color c = new Color(image.getRGB(i, j));
+                grays[i][j] = (c.getRed() * 0.299 + c.getGreen() * 0.587 + c.getBlue() * 0.114) * L / 255;
+            }
+        }
         return new TextureDescriptor();
     }
 
@@ -34,4 +46,77 @@ public class TextureIndexerEngine implements IndexerEngine {
 
         return indexedImages;
     }
+
+    //Co-occurence matrix
+    public double[][] cMatrix0 (double grays[][]){
+        double [][] cGrays = new double[grays.length][grays[0].length];
+        Arrays.fill(cGrays, 0);
+
+        for (int x = 0 ; x < L ; x++){
+            for (int i = 0 ; i < grays.length ; i++){
+                for (int j = 0 ; j < grays[0].length ; j++) {
+                    if (grays[i][j] == x && grays[i][j] == grays[i + 1][j])
+                        cGrays[i][j]++;
+                }
+            }
+        }
+        return cGrays;
+    }
+
+    public double[][] cMatrix45 (double grays[][]){
+        double [][] cGrays = new double[grays.length][grays[0].length];
+        Arrays.fill(cGrays, 0);
+
+        for (int x = 0 ; x < L ; x++){
+            for (int i = 0 ; i < grays.length ; i++){
+                for (int j = 0 ; j < grays[0].length ; j++) {
+                    if (grays[i][j] == x && grays[i][j] == grays[i + 1][j])
+                        cGrays[i][j]++;
+                }
+            }
+        }
+        return cGrays;
+    }
+
+    //Matrix's parameters
+    public static double energie(double grays[][]){
+        double sum = 0;
+        for (int i =0;i<grays.length;i++){
+            for (int j=0;j<grays.length;j++){
+                sum += grays[i][j] *grays[i][j];
+            }
+        }
+        return sum;
+    }
+
+    public static double inertie(double grays[][]){
+        double sum=0;
+        for (int i =0;i<grays.length;i++){
+            for (int j=0;j<grays.length;j++){
+                sum += (i-j)*(i-j)*grays[i][j];
+            }
+        }
+        return sum;
+    }
+
+    public static double entropie(double grays[][]){
+        double sum=0;
+        for (int i =0;i<grays.length;i++){
+            for (int j=0;j<grays.length;j++){
+                sum += grays[i][j] * Math.log(grays[i][j]);
+            }
+        }
+        return sum;
+    }
+
+    public static double mdi(double grays[][]){
+        double sum=0;
+        for (int i =0;i<grays.length;i++){
+            for (int j=0;j<grays.length;j++){
+                sum += 1/((1+ (i-j)*(i-j))*grays[i][j]);
+            }
+        }
+        return sum;
+    }
+
 }
